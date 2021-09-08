7c236fe09a2e8d902af7ba89989b89a2ae064c10
Vilva <alberto.villar@qatium.com>
2021-09-08 16:30:11 +0200

    Iteration one - 1

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,48 @@
-import { wadus } from "./index"
+class Character {
+  isAlive = true
 
-describe("Wadus", () => {
-  it("returns 'wadus'", () => {
-    const expected = "wadus";
+  getHealth(){
+    return 1000
+  }
 
-    const result = wadus();
+  getLevel(){
+    return 1
+  }
 
-    expect(result).toEqual(expected);
+  die(){
+    this.isAlive = false
+  }
+
+  isDead(){
+    return !this.isAlive
+  }
+}
+
+
+describe("Character", () => {
+  it("starts with 1000 health'", () => {
+    const character = new Character()
+
+    expect(character.getHealth()).toEqual(1000);
+  })
+
+  it("starts at level 1", () => {
+    const character = new Character()
+
+    expect(character.getLevel()).toEqual(1);
+  })
+
+  it("starts being alive", () => {
+    const character = new Character()
+
+    expect(character.isDead()).toEqual(false);
+  })
+  
+  it("can be dead", () => {
+    const character = new Character()
+
+    character.die()
+
+    expect(character.isDead()).toEqual(true);
   })
 })
